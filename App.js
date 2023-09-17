e991cfe914a2f761aa056edbcdc4fee4c95d74a6
MallikAdi <101927331+MallikAdi@users.noreply.github.com>
2023-09-17 15:10:18 +0530

    Update App.js
    updated the screens to work with navigation and API-fed charts

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
-import { StatusBar } from 'expo-status-bar'
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import HomePage from './src/screens/HomePageDep'
+import DataFetchingComponent from './src/components/GetDatav2/DataFetch'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import HomeScreen from './src/screens/HomeScreen'
-import TempScreen from './src/screens/parameters/TempScreen'
-import RainScreen from './src/screens/parameters/RainScreen'
 import LoginScreen from './src/components/LoginScreen'
+import HomeScreen from './src/screens/HomeScreen'
+import TempScreen2 from './src/screens/parameters/TempScreenv2'
+import SoilMScreen2 from './src/screens/parameters/SoilMv2'
+import HumidityScreen2 from './src/screens/parameters/HumidityScreenv2'
+import RainScreen2 from './src/screens/parameters/RainScreenv2'
 
 const Stack = createNativeStackNavigator()
 
 const App = () => {
-  //const
-
+  DataFetchingComponent()
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -22,10 +23,10 @@ const App = () => {
           component={HomeScreen}
           options={{ title: 'Welcome' }}
         />
-        <Stack.Screen name="Temp" component={TempScreen} />
-        <Stack.Screen name="Rain" component={RainScreen} />
-        <Stack.Screen name="Humidity" component={RainScreen} />
-        <Stack.Screen name="Soil Moisture" component={RainScreen} />
+        <Stack.Screen name="Temp" component={TempScreen2} />
+        <Stack.Screen name="Rain" component={RainScreen2} />
+        <Stack.Screen name="Humidity" component={HumidityScreen2} />
+        <Stack.Screen name="Soil Moisture" component={SoilMScreen2} />
       </Stack.Navigator>
     </NavigationContainer>
   )
