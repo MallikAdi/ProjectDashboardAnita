bed29f8839dedd3c25ad71a95dabf35830ff09b6
MallikAdi <101927331+MallikAdi@users.noreply.github.com>
2023-09-17 15:15:50 +0530

    Updated parameter screens
    v1 files deleted, only v2 uploaded

---
+++ src/screens/parameters/TempScreenv2.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import { LineChart } from 'react-native-chart-kit'
+import Database from '../../components/GetDatav2/Databasev2'
+
+const TempScreen2 = ({ navigation, route }) => {
+  //const { temperatureData } = Database.Temp
+  const data = {
+    labels: [],
+    datasets: [
+      {
+        data: Database.Temp,
+        color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`, // Line color for the first dataset
+        strokeWidth: 2 // Line width for the first dataset
+      }
+    ]
+  }
+
+  return (
+    <View style={styles.container}>
+      <LineChart
+        data={data}
+        width={300}
+        height={200}
+        yAxisLabel=""
+        chartConfig={{
+          backgroundGradientFrom: 'white',
+          backgroundGradientTo: 'white',
+          decimalPlaces: 0,
+          color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          style: {
+            borderRadius: 16
+          }
+        }}
+        bezier
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+export default TempScreen2
