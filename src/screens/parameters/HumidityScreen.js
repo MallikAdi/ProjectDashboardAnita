29cd8d4ae6a6138a29e73d8b03d3fdae39de6843
MallikAdi <101927331+MallikAdi@users.noreply.github.com>
2023-09-17 15:14:43 +0530

    Delete src/screens/parameters directory

--- src/screens/parameters/HumidityScreen.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-const HumidityScreen = ({ navigation, route }) => {
-  return <Text>This is {route.params.name}'s screen</Text>
-}
-
-export default HumidityScreen
